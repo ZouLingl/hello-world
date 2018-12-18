6c47dfa157001132d5072e9450ea0896ac2ba073
ZouLingl <13817756974@168.com>
2018-12-18 12:07:14 +0800

    123

---
+++ src/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="index">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import'assets/vendors/base/vendors.bundle.css';
+@import'assets/demo/default/base/style.bundle.css';
+
+
+</style>
+<script>
+  // import vendors from 'assets/vendors/base/vendors.bundle.js';
+  // import scripts from 'assets/demo/default/base/scripts.bundle.js';
+</script>
