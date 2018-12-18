68ddc905a00f62891971bf75674d95a3befb6e83
ZouLingl <13817756974@168.com>
2018-12-18 15:48:40 +0800

    1

--- src/Index.vue
+++ src/Index.vue
@@ -3,14 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-<style>
-@import 'assets/vendors/base/vendors.bundle.css';
-@import 'assets/demo/default/base/style.bundle.css';
-@import 'assets/css/blue/theme.css';
-@import 'assets/css/layout.css';
-</style>
-<script>
-import './assets/vendors/base/vendors.bundle.js';
-// import './assets/demo/default/base/scripts.bundle.js';
-</script>
