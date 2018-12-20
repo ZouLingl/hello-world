c9e985be87f4734203d8f2560b01760af16c4f99
ZouLingl <13817756974@168.com>
2018-12-20 10:25:12 +0800

    deleted mer

--- src/Index.vue
+++ src/Index.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="index">
     <router-view/>
+    <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>  
   </div>
+  
 </template>
+
+<style>
+
+</style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
